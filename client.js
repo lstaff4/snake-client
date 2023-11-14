7f2d3d4fc9b7efd857f841b8f23e9f940f94332d
Louis Stafford <louiststafford@gmail.com>
2023-11-14 17:48:04 +0000

    Add name

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server.")
+    conn.write("Name: LTS")
+  });  
+  conn.on('data', (data) => {
+    console.log(`Message from server: ${data}`);
+  });
+  return conn;
+};
+
+module.exports = {
+  connect,
+};
---
+++ play.js
@@ -0,0 +1,6 @@
+const net = require("net");
+const {connect} = require("./client.js")
+
+// establishes a connection with the game server
+console.log("Connecting ...");
+connect();
