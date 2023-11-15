300c7936d0c6f1c44b319dca13004cc5cd1ef75f
Louis Stafford <louiststafford@gmail.com>
2023-11-15 13:57:59 +0000

    Create input.js

--- play.js
+++ play.js
@@ -1,20 +1,8 @@
 const net = require("net");
 const {connect} = require("./client.js")
-
-const handleUserInput = function () {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const {setupInput} = require('./input.js')
 // establishes a connection with the game server
+
 console.log("Connecting ...");
 connect();
+setupInput();
